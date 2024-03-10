f5f8d18a38720408dcbff29bb3e021845e989b34
BhangaleGunjan <120701549+BhangaleGunjan@users.noreply.github.com>
2024-03-10 15:29:00 +0530

    Added starting files

---
+++ script.js
@@ -0,0 +1,31 @@
+function toHomePage() {
+    window.location.href = "index.html";
+}
+
+function toViewMenu() {
+    window.location.href = "viewMenu.html";
+}
+
+function toOrderNow() {
+    window.location.href = "orderNow.html";
+}
+
+function toContactUs() {
+    window.location.href = "contactUs.html";
+}
+
+function toWatchVideo() {
+    window.location.href = "https://youtu.be/sH-juTnjm-Y?feature=shared";
+}
+
+function toTwitter() {
+    window.location.href = "https://www.twitter.com";
+}
+
+function toInstagram() {
+    window.location.href = "https://www.instagram.com";
+}
+
+function toFacebook() {
+    window.location.href = "https://www.facebook.com";
+}
